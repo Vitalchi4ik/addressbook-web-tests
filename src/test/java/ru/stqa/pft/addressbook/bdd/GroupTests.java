857fc9538c16180f8b647685976d3ea785864f55
Roronoa_Zorro <havrilyukvv@gmail.com>
2021-03-29 17:21:44 +0300

    add cucumber test

---
+++ src/test/java/ru/stqa/pft/addressbook/bdd/GroupTests.java
@@ -0,0 +1,9 @@
+package ru.stqa.pft.addressbook.bdd;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions
+public class GroupTests extends AbstractTestNGCucumberTests {
+
+}
